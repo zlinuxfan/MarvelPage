02db386cb4fce9242254d61236546b9d70ba601b
sergey.chikunkov@comodo.com <zlinuxfan@gmail.com>
2023-05-17 10:12:54 -0900

    Makeup: was added infoChar and listChar.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,11 @@
 import Header from "../header/Header";
 import RandomChar from "../randomChar/RandomChar";
+import InfoChar from "../InfoChar/InfoChar";
+import ListChar from "../listChar/ListChar";
 
+import './app.scss';
+
+import decoration from "../../resources/img/bg_asset.png";
 
 const App = () => {
     return (
@@ -8,6 +13,11 @@ const App = () => {
             <Header/>
             <main>
                 <RandomChar/>
+                <div className={"char__content"}>
+                    <ListChar/>
+                    <InfoChar/>
+                </div>
+                <img className={"bg-decoration"} src={decoration} alt=""/>
             </main>
         </div>
     )
